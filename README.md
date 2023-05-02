8ce912643b3ba6097f1d3259fa7527c5810b07dc
devalvesg <gabrieldamasceno881@outlook.com>
2023-05-01 21:17:37 -0300

    Projeto finalizado

---
+++ README.md
@@ -0,0 +1,19 @@
+# Interactive-Card
+Esse projeto foi um desafio do site https://www.frontendmentor.io/challenges. 
+<br>
+
+**Nesse projeto o objetivo e fazer com que o usuário digite as informações que quer colocar no cartão, e após digitados elas serão informadas no cartão, caso haja erros aparecera uma mensagem na tela mostrando aonde está o erro:**
+<br>
+![image](https://user-images.githubusercontent.com/109531207/235550428-5c123901-0ce8-4846-a9dc-963472ee8e58.png)
+<br><br>
+**Conforme as informações forem digitadas de forma correta, sera mostrado uma tela de agradecimentos:**
+<br>
+![image](https://user-images.githubusercontent.com/109531207/235550625-782f4b41-8445-40ef-a832-0c06243e645b.png)
+<br><br>
+**Esse site conta com um design completamente responsivo para smarthphones:**
+<br>
+![image](https://user-images.githubusercontent.com/109531207/235551372-c431bd41-ba4a-4f38-9354-bd7893ef4958.png)
+<br><br>
+<h2>Tecnologias Utilizadas</h2>
+Nesse projeto eu utilizei as tecnologias: <br>
+<h3>HTML, CSS e JavaScript</h3>
---
+++ script.js
@@ -0,0 +1,100 @@
+function TestarDados() {
+    let nome = document.getElementById("cardName").value;
+    document.getElementById("nomeCartao").innerHTML = nome;
+
+    let card_number = document.getElementById("cardNumber").value;
+    const tamanhoCard = 16
+
+    const verificacaoFinalCard = verificarTamanho(card_number, tamanhoCard);
+    if (verificacaoFinalCard == true) {
+        document.getElementById("numCartao").innerHTML = card_number
+        document.getElementById("error").innerHTML = null;
+    }
+    else {
+        document.getElementById("error").innerHTML = "Wrong Formart, minimun 16 numbers"
+    }
+
+    const tamanhoData = 2;
+    let month = document.getElementById("month").value;
+    let year = document.getElementById("year").value;
+    let barra = "/";
+
+    const verificacaoFinalMonth = verificarTamanho(month, tamanhoData);
+    const verificacaoFinalYear = verificarTamanho(year, tamanhoData);
+
+    if (verificacaoFinalMonth == true && verificacaoFinalYear == true && month <= 12) {
+        document.getElementById("dataCard").innerHTML = month + "/" + year;
+        document.getElementById("errorData").innerHTML = null;
+    }
+    else {
+        document.getElementById("errorData").innerHTML = "Incorrect Date"
+    }
+
+
+    const tamanhoCvc = 3;
+    let cvc = document.getElementById("cvc").value;
+
+    const verificacaoFinalCvc = verificarTamanho(cvc, tamanhoCvc);
+
+    if (verificacaoFinalCvc == true) {
+        document.getElementById("cvcCard").innerHTML = cvc;
+        document.getElementById("errorCvc").innerHTML = null;
+    }
+    else {
+        document.getElementById("errorCvc").innerHTML = "Can't be blank"
+    }
+
+    if (verificacaoFinalCard && verificacaoFinalMonth && verificacaoFinalYear && verificacaoFinalCvc && month <= 12) {
+        document.getElementById("info").style.display = 'none';
+        document.getElementById("loadingPage").style.display = 'flex';
+        setTimeout(function () {
+            document.getElementById("loadingPage").style.display = 'none'
+            document.getElementById("thankyou").style.display = 'inline';
+        }, 3000);
+    }
+}
+
+function verificarTamanho(valor, tamanho) {
+    if (valor.length != tamanho) {
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+function Resposta() {
+    let numero = document.getElementById("cardNumber").value;
+    // numero = numero.replace(/(\d{4})/g, "$1 ");
+    document.getElementById("cardNumber").value = numero;
+
+
+    let nome = document.getElementById("cardName").value;
+    nome = nome.toUpperCase();
+    document.getElementById("nomeCartao").innerHTML = nome;
+
+    
+    let card_number = document.getElementById("cardNumber").value;
+    document.getElementById("numCartao").innerHTML = numero
+    
+
+    //Testando se o usúario está digitando a data corretamente
+    let barra = "/";
+    if (document.getElementById("dataCard").value == null) {
+
+    }
+    else {
+        let month = document.getElementById("month").value;
+        let year = document.getElementById("year").value;
+        document.getElementById("dataCard").innerHTML = month + barra + year;
+    }
+
+
+    let cvc = document.getElementById("cvc").value;
+    document.getElementById("cvcCard").innerHTML = cvc;
+}
+
+
+function Continuar() {
+    location.reload();
+}
